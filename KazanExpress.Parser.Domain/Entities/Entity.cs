c27aea79614d9117742f8cd25282431194c10eda
Yury <y.fomenkov@indeepa,com>
2021-08-14 19:54:33 +0300

    Initial

---
+++ KazanExpress.Parser.Domain/Entities/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KazanExpress.Parser.Domain.Entities
+{
+    public class Entity
+    {
+        public DateTime Created { get; private set; } = DateTime.Now;
+        public DateTime Updated { get; private set; } = DateTime.Now;
+        public bool IsDeleted { get; private set; }
+
+        public void SetUpdated()
+        {
+            Updated = DateTime.Now;
+        }
+
+        public void SetInactive()
+        {
+            IsDeleted = true;
+        }
+        
+    }
+}
