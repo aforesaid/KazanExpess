3369228886788a6bc5c5b1a2a080337171330260
Yury <y.fomenkov@indeepa,com>
2021-08-14 21:24:05 +0300

    Реализована модель категории в БД

---
+++ KazanExpress.Parser.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KazanExpress.Parser.Domain.Entities
+{
+    public class CategoryEntity : Entity
+    {
+        private const int ProductIdsLength = 40;
+
+        public CategoryEntity(long categoryId, long productAmount, bool adult, bool eco, string title, string productIds)
+        {
+            Id = categoryId;
+            ProductAmount = productAmount;
+            Adult = adult;
+            Eco = eco;
+            Title = title;
+            ProductIds = productIds;
+        }
+        public long ProductAmount { get; private set; }
+        
+        public bool Adult { get; private set; }
+        
+        public bool Eco { get; private set; }
+        
+        public string Title { get; private set; }
+        
+        [StringLength(ProductIdsLength)]
+        public string ProductIds { get; set; }
+
+        public void UpdateProductAmount(long productAmount)
+        {
+            ProductAmount = productAmount;
+            SetUpdated();
+        }
+    }
+}
--- KazanExpress.Parser.Domain/Entities/Entity.cs
+++ KazanExpress.Parser.Domain/Entities/Entity.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace KazanExpress.Parser.Domain.Entities
 {
     public class Entity
     {
+        [Key]
+        public long Id { get; set; }
+        
         public DateTime Created { get; private set; } = DateTime.Now;
         public DateTime Updated { get; private set; } = DateTime.Now;
         public bool IsDeleted { get; private set; }
