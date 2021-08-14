6d00927716a8374c3064e11f143671feb2f9ea49
Yury <y.fomenkov@indeepa,com>
2021-08-14 21:47:05 +0300

    Добавлены индексы для сущностей Category & Product

--- KazanExpress.Parser.Infrastructure.Data/KazanExpressParserDbContext.cs
+++ KazanExpress.Parser.Infrastructure.Data/KazanExpressParserDbContext.cs
@@ -1,7 +1,38 @@
-﻿namespace KazanExpress.Parser.Infrastructure.Data
+﻿using KazanExpress.Parser.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KazanExpress.Parser.Infrastructure.Data
 {
-    public class KazanExpressParserDbContext
+    public class KazanExpressParserDbContext : DbContext
     {
-        
+        public DbSet<CategoryEntity> Categories { get; protected set; }
+        public DbSet<ProductEntity> Products { get; protected set; }
+        protected KazanExpressParserDbContext() 
+            : base()
+        { }
+
+        protected KazanExpressParserDbContext(DbContextOptions<KazanExpressParserDbContext> options) 
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryEntity>().HasKey(x => x.Id);
+            modelBuilder.Entity<CategoryEntity>().HasIndex(x => x.Id);
+            modelBuilder.Entity<CategoryEntity>().HasIndex(x => x.ProductAmount);
+            modelBuilder.Entity<CategoryEntity>().HasIndex(x => x.Title);
+            modelBuilder.Entity<CategoryEntity>().HasIndex(x => x.ProductIds);
+
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.Id);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.Rating);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.Title);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.CharityCommission);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.FullPrice);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.SellPrice);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.ROrdersQuantity);
+            modelBuilder.Entity<ProductEntity>().HasIndex(x => x.OrdersQuantity);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
